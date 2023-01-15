1c0aa7904b106927bda02d80ad64112cbfdbfc98
Xsenik <xsenik@gmail.com>
2023-01-15 23:28:52 +0200

    Initial commit

---
+++ venv/settings.py
@@ -0,0 +1,12 @@
+WINDOW_WIDTH, WINDOW_HEIGHT = 1280, 720
+
+LAYERS = {
+    'BG' : 0,
+    'BG Detail' : 1,
+    'Level' : 2,
+    'FG Detail Bottom' : 3,
+    'FG Detail Top' : 4,
+    's1': 5,
+    's2': 6,
+    's3':7
+}
